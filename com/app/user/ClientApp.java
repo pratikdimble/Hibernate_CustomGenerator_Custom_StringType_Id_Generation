3878ebba0fa1631d47cdf93b96fd18a8aafb3d02
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-27 10:33:33 +0530

    Create ClientApp.java

---
+++ com/app/user/ClientApp.java
@@ -0,0 +1,48 @@
+package com.app.user;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.app.domain.lucky_draw;
+import com.app.utility.HibernateUtil;
+
+public class ClientApp {
+
+	public static void main(String[] args) 
+	{
+		Session ses = null;
+		lucky_draw coupen = null;
+		Transaction tx = null;
+
+		ses = HibernateUtil.getSession();
+				
+		coupen=new lucky_draw();
+		coupen.setCustomerName("Rohit");
+		coupen.setBillAmount(80000);
+		
+		try {
+			tx = ses.beginTransaction();
+			String id=(String)ses.save(coupen);
+			tx.commit();
+			System.out.println("object is saved");
+			System.out.println("\n\t***Generated ID Value::: "+id);
+		} catch (Exception e) {
+			tx.rollback();
+
+		}
+		ses.close();
+		System.out.print("\n\t\t ==PROGRAMME BY PRATIK DIMBLE==\n\n\n");
+
+		
+
+	}
+
+}
